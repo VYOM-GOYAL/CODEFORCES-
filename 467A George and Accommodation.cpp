355cf041e3bd180878bbbf974f2f25a351170dde
Vyom Goyal <115768839+VYOM-GOYAL@users.noreply.github.com>
2022-11-11 11:25:42 +0530

    Add files via upload

---
+++ 467A George and Accommodation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int count=0;
+    int p,q;
+
+    while(n)
+    {
+        cin>>p>>q;
+
+        if(q-p>=2)
+        {
+            count++;
+        }
+        n--;
+    }
+
+    cout<<count<<endl;
+
+
+    return 0;
+}
