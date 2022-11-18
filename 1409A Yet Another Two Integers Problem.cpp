b87e8d6a0f2265b26eebbaac298ce280f535cbfc
Vyom Goyal <115768839+VYOM-GOYAL@users.noreply.github.com>
2022-11-18 17:16:09 +0530

    Add files via upload

---
+++ 1409A Yet Another Two Integers Problem.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    int count=0;
+
+    while(t--)
+    {
+        int a, b;
+        cin>>a>>b;
+
+        if(a==b)
+        {
+            cout<<"0"<<endl;
+        }
+
+        else
+        {
+            if(abs(a-b)%10==0)
+            {
+                cout<<abs(a-b)/10<<endl;
+            }
+            else
+            {
+                cout<<abs(a-b)/10+1<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
