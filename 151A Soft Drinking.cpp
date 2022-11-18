8cb8dced563e95c08ed4b3f6df2c03a5990cb854
Vyom Goyal <115768839+VYOM-GOYAL@users.noreply.github.com>
2022-11-18 17:47:13 +0530

    Add files via upload

---
+++ 151A Soft Drinking.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, l, c, d, p, nl, np ;
+
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+
+    int total_drinks= k*l;
+    total_drinks= total_drinks/nl;
+
+    int total_slices= c*d;
+    int total_salt= p/np;
+
+    int ans= min(min(total_drinks,total_slices), total_salt)/n;
+    cout<<ans;
+
+    return 0;
+}
