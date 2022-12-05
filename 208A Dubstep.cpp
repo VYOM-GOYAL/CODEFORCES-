d759ad42d44979cf62fdb5dd87456e2f2d36246c
Vyom Goyal <115768839+VYOM-GOYAL@users.noreply.github.com>
2022-12-05 19:47:45 +0530

    Add files via upload

---
+++ 208A Dubstep.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin>>s;
+
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B')
+        {
+            i+=2;
+            cout<<" ";
+        }
+        else
+        {
+            cout<<s[i];
+        }
+    }
+
+    return 0;
+}
