59612715131e65099e50e2eab91611efc471544f
Vyom Goyal <115768839+VYOM-GOYAL@users.noreply.github.com>
2022-12-13 23:32:07 +0530

    Update 426A Sereja and Mugs.cpp

--- 426A Sereja and Mugs.cpp
+++ 426A Sereja and Mugs.cpp
@@ -6,17 +6,17 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, s;                 // n==3       s==4
+    int n, s;                 
     cin>>n>>s;
     int arr[n];
 
     for(int i=0; i<n; i++)
     {
-        cin>>arr[i];         // 4 4 4
+        cin>>arr[i];         
     }
     sort(arr, arr+n);
 
-    int friends= n-1;         // friends == 2
+    int friends= n-1;         
     int ans=0;
 
     for(int i=0; i<friends; i++)
